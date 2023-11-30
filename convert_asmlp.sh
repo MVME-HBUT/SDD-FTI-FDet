a94babdb9eaf745d25719be9e85db9f7e463ea62
P-HL <87471007+P-HL@users.noreply.github.com>
2023-11-30 16:25:04 +0800

    Add files via upload

---
+++ convert_asmlp.sh
@@ -0,0 +1,7 @@
+# python convert_swin_to_d2.py ./pretrained/asmlp_nano_patch4_shift5_224.pth ./pretrained/asmlp_nano_patch4_shift5_224_d2.pth
+
+# python convert_swin_to_d2.py ./pretrained/asmlp_tiny_patch4_shift5_224.pth ./pretrained/asmlp_tiny_patch4_shift5_224_d2.pth
+
+python convert_swin_to_d2.py ./pretrained/asmlp_base_patch4_shift5_224.pth ./pretrained/asmlp_base_patch4_shift5_224_d2.pth
+
+python convert_swin_to_d2.py ./pretrained/asmlp_small_patch4_shift5_224.pth ./pretrained/asmlp_small_patch4_shift5_224_d2.pth
